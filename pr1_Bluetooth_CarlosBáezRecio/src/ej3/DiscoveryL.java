f24d548e65c496cf5ab4a77229c68188f6cf9dc5
Carlos BR <tolo130@hotmail.com>
2020-10-22 12:45:00 +0200

    Se añade ejercicio 3
    Ejercicio 3, busqueda de dispositivos bluettoth filtrando dirección y nombre. En este caso se soluciona añadiendo 4 modos de uso. busqueda normal (como el ejercicio 2) busqueda por dirección, busqueda por nombre y busqueda por dirección y nombre.

--- pr1_Bluetooth_CarlosBáezRecio/src/ej3/DiscoveryL.java
+++ pr1_Bluetooth_CarlosBáezRecio/src/ej3/DiscoveryL.java
@@ -1,20 +1,69 @@
 package ej3;
 
+import java.io.IOException;
+
 import javax.bluetooth.DeviceClass;
 import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.ServiceRecord;
 
 public class DiscoveryL implements javax.bluetooth.DiscoveryListener {
+	
+	private Object remote=null; //Efectivamente, este es el objeto a sincronizar
+	private String lf_dir=null; //Estamos buscando por esta dirección
+	private String lf_name=null; //Estamos buscando por este friendly name
+	private int mode=-1; //Voy a tener 4 posibilidades-> 0->no dir no name, 1->dir no name, 2-> no dir name, 3-> dir name, otherwise-> error
+	
+
+	public DiscoveryL(Object remote, String lf_dir, String lf_name) {
+		this.remote = remote;
+		this.lf_dir = lf_dir;
+		this.lf_name = lf_name;
+		
+		if ((this.lf_dir==null)&&(this.lf_name==null)) {
+			this.mode=0;
+		}
+		if ((this.lf_dir!=null)&&(this.lf_name==null)) {
+			this.mode=1;
+		}
+		if ((this.lf_dir==null)&&(this.lf_name!=null)) {
+			this.mode=2;
+		}
+		if ((this.lf_dir!=null)&&(this.lf_name!=null)) {
+			this.mode=3;
+		}
+	}
+	
 
 	@Override
-	public void deviceDiscovered(RemoteDevice arg0, DeviceClass arg1) {
+	public void deviceDiscovered(RemoteDevice rd, DeviceClass dc) {
 		
-		
+		if (mode==0) {
+			//System.out.println("Mode: "+this.mode+ " Default mode");
+			defaultDiscovery(rd, dc);
+		}
+		else if (mode==1) {
+			//System.out.println("Mode: "+this.mode+ " Address search");
+			discByDir(rd, dc);
+		}
+		else if (mode==2) {
+			//System.out.println("Mode: "+this.mode+ " Name search");
+			discByName(rd, dc);
+		}
+		else if (mode==3) {
+			//System.out.println("Mode: "+this.mode+ " Address + Name search");
+			discByDirName(rd, dc);
+		}
+		else {
+			
+		}
 	}
 
 	@Override
 	public void inquiryCompleted(int arg0) {
-		
+		System.out.println("Search Completed");
+		synchronized (remote) {
+			remote.notifyAll();
+		}
 		
 	}
 
@@ -28,6 +77,60 @@ public class DiscoveryL implements javax.bluetooth.DiscoveryListener {
 	public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {
 		// TODO Auto-generated method stub
 		
+	}
+
+//--------------------------------------------------------------------------------------------
+	
+	//Mode 0: Buscamos todos los dispositivos. No tenemos filtro
+	private void defaultDiscovery (RemoteDevice rd, DeviceClass dc) {
+		try {
+			System.out.println("Device Adress-> "+rd.getBluetoothAddress() + " Friendly Name-> "+rd.getFriendlyName(true));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Mode 1: Filtramos por dirección
+	private void discByDir (RemoteDevice rd, DeviceClass dc) {
+		
+			if (lf_dir.equals(rd.getBluetoothAddress())) {
+				try {
+					System.out.println("Device Adress-> "+rd.getBluetoothAddress() + " Friendly Name-> "+rd.getFriendlyName(true));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} 
+			
+		
+	}
+	
+	//Mode 2: Filtramos por Friendly Name
+	private void discByName (RemoteDevice rd, DeviceClass dc) {
+		try {
+			if (lf_name.equals(rd.getFriendlyName(true))) {
+				System.out.println("Device Adress-> "+rd.getBluetoothAddress() + " Friendly Name-> "+rd.getFriendlyName(true));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//Mode 3: Filtramos por dirección y Friendly Name
+	private void discByDirName (RemoteDevice rd, DeviceClass dc) {
+		try {
+			if ((lf_name.equals(rd.getFriendlyName(true)))&&(lf_dir.equals(rd.getBluetoothAddress()))) {
+				System.out.println("Device Adress-> "+rd.getBluetoothAddress() + " Friendly Name-> "+rd.getFriendlyName(true));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public int getMode() {
+		return this.mode;
 	}
 
 }
--- pr1_Bluetooth_CarlosBáezRecio/src/ej3/Ej3.java
+++ pr1_Bluetooth_CarlosBáezRecio/src/ej3/Ej3.java
@@ -8,17 +8,38 @@ package ej3;
 import javax.bluetooth.BluetoothConnectionException;
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.DiscoveryListener;
 import javax.bluetooth.LocalDevice;
 
 public class Ej3 {
 	
 	public static void main (String args[]) {
 		
+		//Lo uso para crear un hilo extra
 		final Object inquiryCompletedEvent = new Object();
 		
 		try {
-			LocalDevice ld = LocalDevice.getLocalDevice();
-			DiscoveryAgent da = ld.getDiscoveryAgent();
+			LocalDevice ld = LocalDevice.getLocalDevice();   //Ganamos control del dispositivo
+			DiscoveryAgent da = ld.getDiscoveryAgent();		//Obtenemos discovery agent del dispositivo
+			DiscoveryL dl = new DiscoveryL(inquiryCompletedEvent, null, null);				//Aqui creamos discovery listener al que le pasaremos los parametros que nos interesan, en este caso direccion y friendlyname
+			
+			//Ahora comienza la fiesta, sincronizamos el objeto para tener un hilo extra
+			synchronized (inquiryCompletedEvent) {
+				try {
+					boolean start = da.startInquiry(da.GIAC, dl); //Comienzo a buscar. Ojo, startInquiry devuelve 0 en caso de que no haya podido buscar
+					if (start) {
+						System.out.println("Search begins, Mode "+dl.getMode()+"...");
+						inquiryCompletedEvent.wait();
+					}
+					else {
+						System.out.println("Mmmm something went wrong: I couldn't begin any search");
+					}
+					
+				}
+				catch (Exception e) {
+				}
+								
+			}
 		}
 		catch (BluetoothStateException e) {
 			
